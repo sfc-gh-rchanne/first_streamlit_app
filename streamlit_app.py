5f50ac0f2baf89e9c6644577a7464ab0a3b5db81
Ravi Channe <ravi.channe@snowflake.com>
2022-12-08 14:21:55 -0600

    Update streamlit_app.py
    Convert json format to tabular format.

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,4 +27,8 @@ streamlit.dataframe(fruits_to_show)
 #-----------------------------------------------------------
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json()) # json format data write
+# Convert the json format data to tabular by normalizing it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Write the tabular data
+streamlit.dataframe(fruityvice_normalized)
