49ab6c2882a8b08d770dd8c2bf3ff7e267eac453
Ravi Channe <ravi.channe@snowflake.com>
2022-12-09 10:26:28 -0600

    Update streamlit_app.py
    Add a function

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,9 +55,6 @@ streamlit.write('The user entered ', fruit_choice)
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 # my_data_row = my_cur.fetchone()
 
-
-
-
 streamlit.header("The Fruit load list contains:")
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
@@ -70,11 +67,15 @@ if streamlit.button('Get Fruit Load List'):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
-streamlit.stop()
+# streamlit.stop()
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into fruit_load_list values ('from streamlit - 1')")
+    return "'Thanks for adding: " + new_fruit
+
 # Add a second filter input
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding: ', add_my_fruit)
-
-# This will not work, but trying it
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a fruit to the list'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_function)
