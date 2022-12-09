cae55180cfb62d714b1fb24055431e61f93794c5
Ravi Channe <ravi.channe@snowflake.com>
2022-12-09 10:18:43 -0600

    Update streamlit_app.py
    Button to show list

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,18 +49,28 @@ except URLError as e:
 
 streamlit.write('The user entered ', fruit_choice)
 # streamlit.text(fruityvice_response.json()) # json format data write
-streamlit.stop()
-#------------------ Connect to Snowflake -----------------------------------------
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("SELECT * from fruit_load_list")
-# my_data_row = my_cur.fetchone()
-my_data_rows = my_cur.fetchall()
-# streamlit.text("Hello from Snowflake:")
-streamlit.header("The Fruit load list contains:")
-streamlit.dataframe(my_data_rows)
 
+#------------------ Connect to Snowflake -----------------------------------------
+# streamlit.text("Hello from Snowflake:")
+# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+# my_data_row = my_cur.fetchone()
+
+
+
+
+streamlit.header("The Fruit load list contains:")
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("SELECT * from fruit_load_list")
+    return my_cur.fetchall()
+
+# Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
+
+streamlit.stop()
 # Add a second filter input
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding: ', add_my_fruit)
